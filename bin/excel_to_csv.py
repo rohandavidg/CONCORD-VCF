dfa94189873069787f196709ca00d2853b56123b
rohan gnanaolivu <gnanaolivu.rohandavid@mayo.edu>
2017-05-08 10:01:27 -0500

    finished the excel parsing module

--- bin/excel_to_csv.py
+++ bin/excel_to_csv.py
@@ -10,13 +10,12 @@ import csv
 import argparse
 import os
 import collections
-import pprint
-import re
-import logging
+from logging import logging
 
 
-def main(excel_file):
-    
+def main(excel_file, file_name, logging):
+    csv_file = parse_excel_template(file_name, excel_file, logging)
+    return csv_file
 
 
 class convert_excel:
@@ -30,6 +29,7 @@ class convert_excel:
 
     def __iter__(self):
         return self
+
 
     def write_csv(self):
         self.ws = self.wb.worksheets[self.index]
@@ -45,10 +45,21 @@ class convert_excel:
         self.csv_file.close()
 
 
-
     @classmethod
     def remove_csv(cls,csv_file):
         return os.remove(csv_file)
+
+
+def parse_excel_template(file_name, excel_workbook, logging):
+    outfile = file_name + ".csv"
+    try:
+        create_csv_file = convert_excel(excel_workbook, outfile, 0)
+        logger.info('creating a csv from excel file with name {0}'.format(file_name))
+        new_csv_file = create_csv_file.write_csv()
+        create_csv_file.close()
+    except:
+        logger.debug('failed creating excel file, check excel for errors')
+    return outfile
 
 
 if __name__ == "__main__":
@@ -56,5 +67,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-e', dest='excel_file',
                         help='excel file from workbench export', required=True)
+    parser.add_argument('-n', dest='name', 
+                        help='name of the sample or vcf in the excel file', required=True)
     args = parser.parse_args()
-    main(args.excel_file)
+    main(args.excel_file, args.name)
