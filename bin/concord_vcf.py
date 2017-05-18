ab53bb9e685d3173aaee095100f2ef02feaee96c
rohan gnanaolivu <gnanaolivu.rohandavid@mayo.edu>
2017-05-18 16:35:03 -0500

    added concordance vcf script for wes vcf

--- bin/concord_vcf.py
+++ bin/concord_vcf.py
@@ -15,15 +15,25 @@ import plot_dict
 import plot_allele_freq
 
 
+
 log_filename = "CONCORD-VCF"
 
 
 def main(VCF_file_1, sample_name_1,
-         VCF_file_2, sample_name_2, annotate_plot):
+         VCF_file_2, sample_name_2, 
+         annotate_plot, multisample, bior):
     logger = configure_logger(log_filename)
-    VCF_file1_total, VCF_file1_SNP_dict, VCF_file1_indel_dict = parse_vcf.main(VCF_file_1, logger)
-    VCF_file2_total, VCF_file2_SNP_dict, VCF_file2_indel_dict = parse_vcf.main(VCF_file_2, logger)
+    VCF_file1_total, VCF_file1_SNP_dict, VCF_file1_indel_dict = parse_vcf.main(VCF_file_1, multisample, sample_name_1, bior,logger)
+    VCF_file2_total, VCF_file2_SNP_dict, VCF_file2_indel_dict = parse_vcf.main(VCF_file_2, multisample, sample_name_2, bior, logger)
     plot_total = plot_dict.main(VCF_file1_total, VCF_file2_total, "total", logger)
+    if VCF_file1_total:
+        plot_allele_frequency = plot_allele_freq.main(VCF_file1_total, VCF_file2_total, annotate_plot, logger)
+    if VCF_file1_SNP_dict and VCF_file2_SNP_dict:
+        plot_SNP = plot_dict.main(VCF_file1_SNP_dict, VCF_file2_SNP_dict, "SNP", logger)
+    if VCF_file1_indel_dict and VCF_file2_indel_dict:
+        plot_indel = plot_dict.main(VCF_file1_indel_dict, VCF_file2_indel_dict, "INDEL", logger)
+    else:
+        logger.debug('output missing')
 
 
 if __name__ == "__main__":
@@ -39,7 +49,13 @@ if __name__ == "__main__":
                         help='name of the sample or vcf in the VCF file', required=True)
     parser.add_argument('-a', dest='annotate_plot', action="store_true",
                         help='flag to turn on or turn off plot annotation with varaint')
+    parser.add_argument('-m', dest='multisample', action="store_true",
+                        help='flag to turn on if dealing with multi sample vcf')
+    parser.add_argument('-b', dest='bior', action='store_true',
+                        help='flag to set if vcf is run through bior')
     args = parser.parse_args()
     main(args.VCF_file_1, args.sample_name_1,
-         args.VCF_file_2, args.sample_name_2, args.annotate_plot)
+         args.VCF_file_2, args.sample_name_2, 
+         args.annotate_plot, args.multisample,
+         args.bior)
 
--- bin/parse_vcf.py
+++ bin/parse_vcf.py
@@ -11,11 +11,26 @@ import csv
 from do_logging import configure_logger
 from collections import defaultdict
 import pprint
+import subprocess 
+
+vcf_fixup = "/biotools/biotools/vcflib/2015_3_20/bin/vcffixup"
+vcf_remove = "/dlmp/sandbox/cgslIS/rohan/LPEA_CAD_VCF_UTILITIES/VCF_remove.pl"
+perl = "/usr/local/biotools/perl/5.10.1/bin/perl"
+deannotate = "1000genomes_20130502_GRCh37_nodups.INFO.AC,1000genomes_20130502_GRCh37_nodups.INFO.AF,1000genomes_20130502_GRCh37_nodups.INFO.AFR_AF,1000genomes_20130502_GRCh37_nodups.INFO.AMR_AF,1000genomes_20130502_GRCh37_nodups.INFO.AN,1000genomes_20130502_GRCh37_nodups.INFO.EAS_AF,1000genomes_20130502_GRCh37_nodups.INFO.EUR_AF,1000genomes_20130502_GRCh37_nodups.INFO.SAS_AF,16-CHMKH-A-02-00_GT,17-A3039-A-03-00_GT,17-GL830-A-02-00_GT,AC,ACMG_genes_Dec_2016.name,AF,AN,BlatED.ED,CAVA_ALTANN,CAVA_ALTCLASS,CAVA_ALTSO,CAVA_CLASS,CAVA_CSN,CAVA_ENST,CAVA_GENE,CAVA_IMPACT,CAVA_LOC,CAVA_SO,CAVA_TRINFO,CAVA_TYPE,Clinvar_20160515_GRCh37.all_submitters,Clinvar_20160515_GRCh37.all_traits,Clinvar_20160515_GRCh37.clinical_significance,Clinvar_20160515_GRCh37.conflicted,Clinvar_20160515_GRCh37.pathogenic,Clinvar_20160515_GRCh37.review_status,CompoundHet,CompoundHetEffects,culprit,dbNSFP_v3a_GRCh37.CaddPhred,dbNSFP_v3a_GRCh37.LrtPred,dbNSFP_v3a_GRCh37.MetalrPred,dbNSFP_v3a_GRCh37.MetalrRankscore,dbNSFP_v3a_GRCh37.MetasvmRankscore,dbNSFP_v3a_GRCh37.Polyphen2HdivPred,dbNSFP_v3a_GRCh37.Polyphen2HdivRankscore,dbNSFP_v3a_GRCh37.ProveanConvertedRankscore,dbNSFP_v3a_GRCh37.ProveanPred,dbNSFP_v3a_GRCh37.SiftConvertedRankscore,dbNSFP_v3a_GRCh37.SiftPred,dbNSFP_v3a_GRCh37.Vest3Rankscore,dbSNP139.ID,dbSNP_142_GRCh37p13.ID,DP,ESP_V2_GRCh37.AA._maf,ESP_V2_GRCh37.ALL._maf,ESP_V2_GRCh37.EA._maf,ExAC_r03_GRCh37_nodups.INFO.AC_AFR,ExAC_r03_GRCh37_nodups.INFO.AC_AMR,ExAC_r03_GRCh37_nodups.INFO.AC_EAS,ExAC_r03_GRCh37_nodups.INFO.AC_FIN,ExAC_r03_GRCh37_nodups.INFO.AC_Hemi,ExAC_r03_GRCh37_nodups.INFO.AC_Het,ExAC_r03_GRCh37_nodups.INFO.AC_Hom,ExAC_r03_GRCh37_nodups.INFO.AC_NFE,ExAC_r03_GRCh37_nodups.INFO.AC_SAS,ExAC_r03_GRCh37_nodups.INFO.AF,ExAC_r03_GRCh37_nodups.INFO.AN_NFE,ExAC_r03_GRCh37_nodups.INFO.Hemi_AFR,ExAC_r03_GRCh37_nodups.INFO.Hemi_AMR,ExAC_r03_GRCh37_nodups.INFO.Hemi_EAS,ExAC_r03_GRCh37_nodups.INFO.Hemi_FIN,ExAC_r03_GRCh37_nodups.INFO.Hemi_NFE,ExAC_r03_GRCh37_nodups.INFO.Hemi_OTH,ExAC_r03_GRCh37_nodups.INFO.Hemi_SAS,ExperienceDB.ASSAY,ExperienceDB.CLASS,ExperienceDB.NOTES,ExperienceDB.Submitter,HGMD_2016Q1_GRCh37_nodups.CLASS,HGMD_2016Q1_GRCh37_nodups.PHEN,HGMD_2016Q1_GRCh37_nodups.PubMed,InGeneList,InheritancePattern,LOF,MQ,MQ0,NMD,NS,OMIM_20160427_GRCh37p13_EnsemblGTF_75.Phenotypes,PGx_variants.name,snpeff.Amino_Acid_Change,snpeff.Amino_Acid_length,snpeff.Codon_Change,snpeff.Effect_Impact,snpeff.ERRORS,snpeff.Exon_Rank,snpeff.Functional_Class,snpeff.Gene_Coding,snpeff.Gene_Name,snpeff.Genotype_Number,snpeff.Transcript_BioType,snpeff.Transcript_ID,Link_dbSNP,Link_GoogleSearch,Link_GeneCards,Link_Pubmed,Link_IGV,href"
 
 
-def main(vcf_file, logger):
-    vcf_total_dict, vcf_snp_dict, vcf_indel_dict  = vcf_to_dict(vcf_file, logger)
-    return vcf_total_dict, vcf_snp_dict, vcf_indel_dict
+def main(vcf_file, multisample, sample_name, bior, logger):
+    if bior:
+        vcf_fixup_file = convert_vcf_format(vcf_file, logger)
+        if multisample:
+            vcf_total_dict, vcf_snp_dict, vcf_indel_dict  = vcf_to_dict_multisample(vcf_fixup_file, logger, sample_name)
+            return vcf_total_dict, vcf_snp_dict, vcf_indel_dict
+    if multisample:
+        vcf_total_dict, vcf_snp_dict, vcf_indel_dict  = vcf_to_dict_multisample(vcf_file, logger, sample_name)
+        return vcf_total_dict, vcf_snp_dict, vcf_indel_dict
+    else:
+        vcf_total_dict, vcf_snp_dict, vcf_indel_dict = vcf_to_dict(vcf_file, logger)
+        return vcf_total_dict, vcf_snp_dict, vcf_indel_dict
 
 
 def vcf_to_dict(vcf_file, logger):
@@ -23,7 +38,7 @@ def vcf_to_dict(vcf_file, logger):
     indel_dict = {}
     total_dict = {}
     vcf_reader = vcf.Reader(open(vcf_file, 'r'))
-    for record in vcf_reader:
+    for record in vcf_reader:        
         create_dict = multi_allelic(record.CHROM, record.POS, record.REF, record.ALT, record.samples[0]['GT'],
                                    record.samples[0]['AD'], record.samples[0]['DP'], logger)
         total_dict.update(create_dict)
@@ -38,18 +53,57 @@ def vcf_to_dict(vcf_file, logger):
         else:
             logger.warning('variant found that is not an indel or snp {0}:{1}{2}>{3}'.format(record.CHROM,  record.POS, record.REF, record.ALT))
     return total_dict, snp_dict, indel_dict
-#        elif record.is_transition:
-#            create_dict = multi_allelic(record.CHROM, record.POS, record.REF, record.ALT, record.samples[0]['GT'],
-#                                        record.samples[0]['AD'], record.samples[0]['DP'], logger)
-#            del_dict.update(create_dict)
-#        elif record.is_deletion:
-#            create_dict = multi_allelic(record.CHROM, record.POS, record.REF, record.ALT, record.samples[0]['GT'],
-#                                        record.samples[0]['AD'], record.samples[0]['DP'], logger)
-#            transition_dict.update(create_dict)
-#        else:
-#            pass
-
     
+
+def convert_vcf_format(vcf_file, logger):
+    outfile1 = ".".join(vcf_file.split('.')[:-1]) + ".deannotated.vcf"
+    outfile2 = ".".join(vcf_file.split('.')[:-1]) + ".deannotated.vcffixup.vcf" 
+    cmd = perl + " " + vcf_remove + " -v " + vcf_file + " -o " + deannotate + " > " + outfile1
+    logger.info('running cmd {0}'.format(cmd))
+    print cmd
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out = p.communicate()
+    cmd2 = vcf_fixup + " " + outfile1 + " > " + outfile2
+    print cmd2
+    p2 = subprocess.Popen(cmd2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print outfile2
+    return outfile2
+
+
+def vcf_to_dict_multisample(vcf_file, logger, sample_name):
+    snp_dict = {}
+    indel_dict = {}
+    total_dict = {}
+    vcf_reader = vcf.Reader(open(vcf_file, 'r'))
+    print sample_name
+    print vcf_reader.samples
+    sample_index = vcf_reader.samples.index(sample_name)
+    for record in vcf_reader:        
+        try:
+            if record.samples[sample_index]['DP'] and record.samples[sample_index]['AD']:
+                key, value = mutli_sample_dict(record.CHROM, record.POS, record.REF, record.ALT, record.samples[sample_index]['GT'],
+                                               record.samples[sample_index]['AD'], record.samples[sample_index]['DP'], logger)                
+                total_dict[key] = value
+                if record.is_snp:
+                    key, value = mutli_sample_dict(record.CHROM, record.POS, record.REF, record.ALT, record.samples[sample_index]['GT'],
+                                                   record.samples[sample_index]['AD'], record.samples[sample_index]['DP'], logger)
+                    snp_dict[key] = value
+                elif record.is_indel:
+                    key, value = mutli_sample_dict(record.CHROM, record.POS, record.REF, record.ALT, record.samples[sample_index]['GT'],
+                                                   record.samples[sample_index]['AD'], record.samples[sample_index]['DP'], logger)
+                    indel_dict[key] = value
+                else:
+                    logger.warning('variant found that is not an indel or snp {0}:{1}{2}>{3}'.format(record.CHROM,  record.POS, record.REF, record.ALT))
+        except ValueError:
+            pass
+    return total_dict, snp_dict, indel_dict
+
+
+def mutli_sample_dict(chrom, pos, ref, alt_list, GT, AD, DP, logger):
+    genomic_coord = str(chrom) + ":g." + str(pos) + str(ref) + ">" + str(alt_list[0])
+    value = GT, AD, DP, compute_AF(AD[1], DP)
+    return genomic_coord, value
+
 
 def compute_AF(AD, DP):
     AF = float(AD)/float(DP)
@@ -88,4 +142,12 @@ def multi_allelic(chrom, pos, ref, alt_list, GT, AD, DP, logger):
 
 
 if __name__ == "__main__":
-    main(vcf_file, logger)
+#    import argparse
+#    from do_logging import configure_logger    
+#    parser = argparse.ArgumentParser(description=__doc__)
+#    parser.add_argument('-v', dest='vcf_file',
+#                        help='excel file from workbench export', required=True)
+#    args = parser.parse_args()
+#    logger = configure_logger('something')
+#    main(args.vcf_file, logger)
+    main(vcf_file, multi_sample, sample_name, bior, logger)
