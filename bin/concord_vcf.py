b1b33ea455354259791883dff1cfd7268666af02
rohan gnanaolivu <gnanaolivu.rohandavid@mayo.edu>
2017-05-18 10:03:50 -0500

    code for vcf inputs

---
+++ bin/concord_vcf.py
@@ -0,0 +1,23 @@
+#!/dlmp/sandbox/cgslIS/rohan/Python-2.7.11/python
+
+"""
+check concordance between VCF
+"""
+
+import vcf
+import os
+import csv
+from do_logging import configure_logger()
+from collections import defaultdict
+import pprint
+
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+    import argparse
+    main()
+
