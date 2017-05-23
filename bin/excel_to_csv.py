a93078dd71beda2a04a40881c8576bd817a9affb
rohan gnanaolivu <gnanaolivu.rohandavid@mayo.edu>
2017-05-23 15:31:19 -0500

    added code to support 4 samples

--- bin/excel_to_csv.py
+++ bin/excel_to_csv.py
@@ -1,14 +1,14 @@
 #!/dlmp/sandbox/cgslIS/rohan/Python-2.7.11/python
 
 """
-This scripts converts an excel to csv
+This script runs concordance for CLC 
+samples
 """
 
 import os
 import openpyxl
 import csv
 import argparse
-import os
 import collections
 from do_logging import configure_logger
 from csv_to_dict import parse_csv
@@ -16,22 +16,11 @@ import pandas as pd
 import plot_dict
 import plot_allele_freq
 
-log_filename = "CONCORD-VCF"
 
-def main(excel_file_1, sample_name_1, excel_file_2, sample_name_2, annotate_plot):
+def main(excel_file, sample_name, logger):
     logger = configure_logger(log_filename)
-    csv_file_1 = parse_excel_template(sample_name_1, excel_file_1, logger)
-    csv_file_2 = parse_excel_template(sample_name_2, excel_file_2, logger)
-    vcf_dict_1, SNP_dict_1, indel_dict_1 = parse_csv(csv_file_1)
-    vcf_dict_2, SNP_dict_2, indel_dict_2 = parse_csv(csv_file_2)
-    plot_total = plot_dict.main(vcf_dict_1, vcf_dict_2, "total", logger)
-    print annotate_plot
-    plot_allele_frequency = plot_allele_freq.main(vcf_dict_1, vcf_dict_2, sample_name_1, sample_name_2, annotate_plot, logger)
-    if SNP_dict_1 and SNP_dict_2:
-        plot_SNP = plot_dict.main(SNP_dict_1, SNP_dict_2, "SNP", logger)
-    if indel_dict_1 and indel_dict_2:
-        plot_indel = plot_dict.main(indel_dict_1, indel_dict_2, "INDEL", logger)
-
+    csv_file = parse_excel_template(sample_name, excel_file, logger)
+    return csv_file
 
 
 class convert_excel:
@@ -70,30 +59,13 @@ class convert_excel:
 
 def parse_excel_template(file_name, excel_workbook, logger):
     outfile = file_name + ".csv"
-#    try:
     create_csv_file = convert_excel(excel_workbook, outfile, 0)
     logger.info('creating a csv from excel file with name {0}'.format(file_name))
     new_csv_file = create_csv_file.write_csv()
     create_csv_file.close()
-#    except:
     logger.debug('failed creating excel file, check excel for errors')
     return outfile
 
 
-if __name__  ==  "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-FE', dest='excel_file_1',
-                        help='excel file from workbench export', required=True)
-    parser.add_argument('-FS', dest='sample_name_1', 
-                        help='name of the sample or vcf in the excel file', required=True)
-    parser.add_argument('-SE', dest='excel_file_2',
-                        help='excel file from workbench export', required=True)
-    parser.add_argument('-SS', dest='sample_name_2',
-                        help='name of the sample or vcf in the excel file', required=True)
-    parser.add_argument('-a', dest='annotate_plot', action="store_true",
-                        help='flag to turn on or turn off plot annotation with varaint')
-    args = parser.parse_args()
-    main(args.excel_file_1, args.sample_name_1, args.excel_file_2, 
-         args.sample_name_2, args.annotate_plot)
-    
+if __name__ == "__main__":
+    main(excel_file, sample_name, logger)
--- bin/plot_dict.py
+++ bin/plot_dict.py
@@ -17,67 +17,84 @@ import scipy.stats as stats
 import logging
 import shlex
 
-def main(total_dict1, total_dict2, check_name, logger):
-    VCF_overlap, first_vcf_unique, second_vcf_unique = compare_vcf(total_dict1, total_dict2, check_name, logger)
-    if second_vcf_unique > 0 and first_vcf_unique > 0 and VCF_overlap > 0:
-        get_image = plot_differences(VCF_overlap, first_vcf_unique, second_vcf_unique,0.5, 0.5, 1.1, check_name)
-    elif second_vcf_unique == 0 and first_vcf_unique == 0:
-        get_image = plot_differences(VCF_overlap, first_vcf_unique, second_vcf_unique, 0, 0, 1.1, check_name)
-    elif second_vcf_unique == 0 and first_vcf_unique > 0:
-        get_image = plot_differences(VCF_overlap, first_vcf_unique, second_vcf_unique, 0.5, 0, 1.1, check_name)
-    elif VCF_overlap == 0:
-        get_image = plot_differences(VCF_overlap, first_vcf_unique, second_vcf_unique, 0.8, 0.8, 0, check_name)
-    else:
-        get_image = plot_differences(VCF_overlap, first_vcf_unique, second_vcf_unique, 0, 0.8, 1.1,check_name)
+def main(total_dict1, total_dict2, total_dict3, total_dict4,
+         sample_name1, sample_name2, sample_name3, sample_name4,
+         check_name, logger):
 
-def compare_vcf(new_dict, old_dict, analysis_check, logger):
-    with open(analysis_check + '_variants_log.txt', 'w') as fout, open (analysis_check + '_First_input_variant_missing_list.txt', 'w') as vout, open(analysis_check + '_second_input_variant_missing_list.txt', 'w') as sout:
+
+def compare_vcf(first_dict, second_dict, third_dict, fouth_dict, 
+                sample_name1, sample_name2, sample_name3, sample_name4,
+                analysis_check, logger):
+    sample1_sample2_outfile = "Total_missing_report_" + sample_name1 + "_"+ sample_name2 + ".txt"
+    sample1_sample3_outfile = "Total_missing_report_" + sample_name1 + "_"+ sample_name3 + ".txt"
+    sample1_sample4_outfile = "Total_missing_report_" + sample_name1 + "_"+ sample_name4 + ".txt"
+    sample2_sample3_outfile = "Total_missing_report_" + sample_name2 + "_"+ sample_name3 + ".txt"
+    sample2_sample4_outfile = "Total_missing_report_" + sample_name2 + "_"+ sample_name4 + ".txt"
+    sample3_sample4_outfile = "Total_missing_report_" + sample_name3 + "_"+ sample_name4 + ".txt"
+    sample1_sample2_overlap, samplel_unique, sample2_unique = check_missing_in_dict(first_dict, second_dict, sample_name1, sample_name2, sample1_sample2_outfile)
+    create_venn_plots(sample1_sample2_overlap, samplel_unique, sample2_unique, analysis_check, sample_name1, sample_name2, logger)
+    if third_dict:
+        sample1_sample3_overlap, samplel_unique, sample3_unique = check_missing_in_dict(first_dict, third_dict, sample_name1, sample_name3, sample1_sample3_outfile)
+        create_venn_plots(sample1_sample3_overlap, samplel_unique, sample3_unique, analysis_check, sample_name1, sample_name3, logger)
+        sample2_sample3_overlap, sample2_unique, sample3_unique= check_missing_in_dict(second_dict, third_dict, sample_name2, sample_name3, sample2_sample3_outfile)
+        create_venn_plots(sample2_sample3_overlap, sample2_unique, sample3_unique, analysis_check, sample_name2, sample_name3, logger)
+    if fouth_dict:
+        sample1_sample4_overlap, samplel_unique, sample4_unique = check_missing_in_dict(first_dict, fourth_dict, sample_name1, sample_name4, sample1_sample4_outfile)
+        create_venn_plots(sample1_sample4_overlap, sample1_unique, sample4_unique, analysis_check, sample_name1, sample_name4, logger)
+        sample2_sample4_overlap, sample2_unique, sample4_unique = check_missing_in_dict(second_dict, fourth_dict, sample_name2, sample_name4, sample2_sample4_outfile)
+        create_venn_plots(sample2_sample4_overlap, sample2_unique, sample4_unique, analysis_check, sample_name2, sample_name4, logger)
+        sample3_sample4_overlap, sample4_unique, sample4_unique = check_missing_in_dict(third_dict, fourth_dict, sample_name3, sample_name4, sample3_sample4_outfile)
+        create_venn_plots(sample3_sample4_overlap, sample3_unique, sample4_unique, analysis_check, sample_name3, sample_name4, logger)
+
+
+def create_venn_plots(VCF_overlap, first_vcf_unique, second_vcf_unique, check_name, sample1, sample2, logger):
+    if second_vcf_unique > 0 and first_vcf_unique > 0 and VCF_overlap > 0:
+        get_image = plot_differences(VCF_overlap, first_vcf_unique, second_vcf_unique,0.5, 0.5, 1.1, check_name, sample1, sample2)
+    elif second_vcf_unique == 0 and first_vcf_unique == 0:
+        get_image = plot_differences(VCF_overlap, first_vcf_unique, second_vcf_unique, 0, 0, 1.1, check_name, sample1, sample2)
+    elif second_vcf_unique == 0 and first_vcf_unique > 0:
+        get_image = plot_differences(VCF_overlap, first_vcf_unique, second_vcf_unique, 0.5, 0, 1.1, check_name, sample1, sample2)
+    elif VCF_overlap == 0:
+        get_image = plot_differences(VCF_overlap, first_vcf_unique, second_vcf_unique, 0.8, 0.8, 0, check_name, sample1, sample2)
+    else:
+        get_image = plot_differences(VCF_overlap, first_vcf_unique, second_vcf_unique, 0, 0.8, 1.1,check_name, sample1, sample2)
+
+
+def check_missing_in_dict(some_dict1, some_dict2, sample_name1, sample_name2,  outfile):
+    with open(analysis_check + '_variants_log.txt', 'wa+') as fout, open(outfile , 'w') as fout:
         overlap = 0
-        new_vcf_unique = 0
-        old_vcf_unique = 0
-        new_variants = len(new_dict)
-        old_variants = len(old_dict)
-        for k, v in new_dict.items():
+        first_vcf_unique = 0
+        second_vcf_unique = 0
+        first_variants = len(some_dict1)
+        second_variants = len(some_dict2)
+        for k, v in some_dict1.items():
             try:
-                if old_dict[k]:
+                if some_dict2[k]:
                     overlap += 1
                 else:
-                    new_vcf_unique += 1
-                    vout.write(k + '\n')
+                    first_vcf_unique += 1
+                    fout.write(k + '\n')
             except KeyError:
-                new_vcf_unique += 1
-                vout.write('Genomic' + '\t' +  str(k) + '\t' + "\t".join(str(i) for i in v).strip('[]') + '\n')
-
-        for k, v in old_dict.items():
-            try:
-                if new_dict[k]:
-                    pass
-                else:                        
-                    old_vcf_unique += 1
-                    sout.write(k + '\n')
-            except KeyError:
-                old_vcf_unique += 1
-                sout.write('Genomic' + '\t' + str(k) + '\t' + "\t".join(str(i) for i in v).strip('[]') + '\n')
-
+                first_vcf_unique += 1
+                fout.write('Genomic' + '\t' +  str(k) + '\t' + "\t".join(str(i) for i in v).strip('[]') + '\n')
         print "overlap: " + str(overlap)
-        fout.write("Total variants that overlap with 1st input and 2nd input " + str(overlap) + "\n")
-        print "Total variant in 1st vcf input: " + str(new_variants)
-        fout.write("unique only to 1st vcf input: " + str(new_vcf_unique) + '\n')
-        print "unique only to 1st vcf input: " + str(new_vcf_unique)
-        fout.write("Total variants in 2nd vcf input: " + str(old_variants) + '\n')
-        print "Total variant in 2nd vcf input: " + str(old_variants)
-        fout.write("unique only to 2nd vcf input: " + str(old_vcf_unique) + '\n')
-        print "unique only to 2nd vcf: " + str(old_vcf_unique)
-        return overlap, new_vcf_unique, old_vcf_unique
+        fout.write("Total variants that overlap with {0} and {1} is {2}".format(sample_name1, sample_name2, str(overlap)) + "\n")
+        print "Total variant in " + sample_name1 +  " vcf input: " + str(first_variants)
+        fout.write("unique only to " + sample_name1 + " vcf input: " + str(first_vcf_unique) + '\n')
+        print "unique only to " + sample_name1 +" vcf input: " + str(first_vcf_unique)
+        fout.write("Total variants in "+ sample_name2 + " input: " + str(second_variants) + '\n')
+        print "Total variant in " + sample_name2 + " input: " + str(second_variants)
+        fout.write("unique only to " + sample_name2 + " input: " + str(second_vcf_unique) + '\n')
+        print "unique only to " + sample_name2 + " " + str(second_vcf_unique)
+        return overlap, first_vcf_unique, second_vcf_unique
 
 
-
-def plot_differences(overlap, new_vcf_unique, old_vcf_unique, new_error_size, old_error_size, not_common_error_size, analysis_name):
+def plot_differences(overlap, first_vcf_unique, second_vcf_unique, new_error_size, old_error_size, not_common_error_size, analysis_name, sample_name1, sample_name2):
     overlap_text = str(overlap) + " overlapping"
-    new_vcf_txt = str(new_vcf_unique) + " unique first var"
-    old_vcf_txt = str(old_vcf_unique) + " unique second var"
+    new_vcf_txt = str(first_vcf_unique) + " unique " + sample_name1
+    old_vcf_txt = str(second_vcf_unique) + " unique " + sample_name2
     plt.figure(figsize=(8,8))
-    v = venn2(subsets = {'10': new_error_size, '01': old_error_size, '11': not_common_error_size}, set_labels = ('1st VCF INPUT', '2nd VCF INPUT'))
+    v = venn2(subsets = {'10': new_error_size, '01': old_error_size, '11': not_common_error_size}, set_labels = (sample_name1, sample_name2))
     v.get_patch_by_id('10').set_alpha(0.75)
     v.get_patch_by_id('10').set_color('red')
     v.get_patch_by_id('01').set_alpha(0.75)
@@ -116,7 +133,7 @@ def plot_differences(overlap, new_vcf_unique, old_vcf_unique, new_error_size, ol
         plt.annotate('2nd VCF INPUT', xy = v.get_label_by_id('01').get_position(), xytext = (30,-70), size = 'xx-large',
                      ha = 'center', textcoords = 'offset points', bbox = dict(boxstyle = 'round, pad = 0.5', fc = 'lime', alpha = 0.3),
                      arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad = -0.5',color = 'gray'))
-        plt.savefig(analysis_name + "_VCF_VARIANT_CHECK.png")
+        plt.savefig(analysis_name + "_" + sample_name1 + "_" + sample_name2 + "_VCF_VARIANT_CHECK.png")
 
 
 if __name__ == "__main__":
